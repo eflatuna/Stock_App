9143d2ddd0f5e7506f4de54e5dbb28b0c9d728b0
esra <esrabaskaya84@gmail.com>
2024-04-19 09:11:46 +0200

    stockSlice is created

---
+++ RC-24-29-stock-app/inclass-stockapp-2-5/src/features/stockSlice.jsx
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stockSlice = createSlice({
+	name: "auth",
+
+	initialState: {
+		currentUser: null,
+		loading: false,
+		error: false,
+		token: null,
+	},
+	reducers: {
+		fetchStart: (state) => {
+			state.loading = true;
+			state.error = false;
+		},
+
+		fetchFail: (state) => {
+			state.loading = false;
+			state.error = true;
+		},
+	},
+});
+
+export const {
+	fetchStart,
+
+	fetchFail,
+} = stockSlice.actions;
+export default stockSlice.reducer;
