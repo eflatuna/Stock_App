7f252138e55073782e4ac9f4396164abb67ad236
esra <esrabaskaya84@gmail.com>
2024-04-27 21:14:37 +0200

    edited

--- RC-24-29-stock-app/inclass-stockapp-2-5/src/components/Dashboard/Charts.jsx
+++ RC-24-29-stock-app/inclass-stockapp-2-5/src/components/Dashboard/Charts.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import { AreaChart, Grid } from "@tremor/react";
+import { useSelector } from "react-redux";
 
-const Charts = () => {
-	return <div>Charts</div>;
-};
+const chartdata = [
+	{
+		date: "Jan 22",
+		SemiAnalysis: 2890,
+		"The Pragmatic Engineer": 2338,
+	},
+	{
+		date: "Feb 22",
+		SemiAnalysis: 2756,
+		"The Pragmatic Engineer": 2103,
+	},
+	{
+		date: "Mar 22",
+		SemiAnalysis: 3322,
+		"The Pragmatic Engineer": 2194,
+	},
+	{
+		date: "Apr 22",
+		SemiAnalysis: 3470,
+		"The Pragmatic Engineer": 2108,
+	},
+	{
+		date: "May 22",
+		SemiAnalysis: 3475,
+		"The Pragmatic Engineer": 1812,
+	},
+	{
+		date: "Jun 22",
+		SemiAnalysis: 3129,
+		"The Pragmatic Engineer": 1726,
+	},
+	{
+		date: "Jul 22",
+		SemiAnalysis: 3490,
+		"The Pragmatic Engineer": 1982,
+	},
+	{
+		date: "Aug 22",
+		SemiAnalysis: 2903,
+		"The Pragmatic Engineer": 2012,
+	},
+	{
+		date: "Sep 22",
+		SemiAnalysis: 2643,
+		"The Pragmatic Engineer": 2342,
+	},
+	{
+		date: "Oct 22",
+		SemiAnalysis: 2837,
+		"The Pragmatic Engineer": 2473,
+	},
+	{
+		date: "Nov 22",
+		SemiAnalysis: 2954,
+		"The Pragmatic Engineer": 3848,
+	},
+	{
+		date: "Dec 22",
+		SemiAnalysis: 3239,
+		"The Pragmatic Engineer": 3736,
+	},
+];
 
-export default Charts;
+const dataFormatter = (number) =>
+	`$${Intl.NumberFormat("us").format(number).toString()}`;
+
+export default function Charts() {
+	const { sales, purchases } = useSelector((state) => state.stock);
+	const salesData = sales.map((item) => ({
+		date: new Date(item.createdAt).toLocaleDateString(),
+		sale: item.amount,
+	}));
+	const purchasesData = purchases.map((item) => ({
+		date: new Date(item.createdAt).toLocaleString(),
+		purchase: item.amount,
+	}));
+	return (
+		<Grid container spacing={3} mt={3}>
+			<Grid item xs={12} md={6}>
+				<AreaChart
+					className="h-80"
+					data={salesData}
+					index="date"
+					categories={["sale"]}
+					colors={["indigo", "rose"]}
+					valueFormatter={dataFormatter}
+					yAxisWidth={60}
+					onValueChange={(v) => console.log(v)}
+				/>
+				<AreaChart
+					className="h-80"
+					data={purchasesData}
+					index="date"
+					categories={["purchase"]}
+					colors={["fuchsia"]}
+					valueFormatter={dataFormatter}
+					yAxisWidth={60}
+					onValueChange={(v) => console.log(v)}
+				/>
+			</Grid>
+		</Grid>
+	);
+}
--- RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Home.jsx
+++ RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import PageHeader from "../components/Commons/PageHeader";
+import KpiCards from "../components/Dashboard/KpiCards";
+import Charts from "../components/Dashboard/Charts";
+import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+	const { getPurcSales } = useStockCall();
+	const { loading, error } = useSelector((state) => state.stock);
+	useEffect(() => {
+		getPurcSales();
+	}, []);
+
+	if (loading) return <h2>Loading...</h2>;
+	if (error) return <h2>Something went wrong</h2>;
+
 	return (
 		<Container maxWidth={"xl"}>
 			<PageHeader text="Dashboard" />
+			<KpiCards />
+			<Charts />
 		</Container>
 	);
 };
